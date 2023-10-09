0b02b21b5444010b13465eceee5bfd5990453f05
Nathan Pennington <nathantp@gmail.com>
2023-10-09 16:13:06 -0500

    10 functions

--- solutions.js
+++ solutions.js
@@ -21,3 +21,74 @@
  * concat
  */
 
+const isTrue = (anyInput) => {
+    if (Boolean (anyInput) === true && anyInput === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const isFalse = (anyInput) => {
+    if (Boolean (anyInput) === false && anyInput === false) {
+        return true;
+
+    } else {
+        return false;
+    }
+}
+
+const not = (anyInput) => {
+    return !(Boolean(anyInput));
+
+}
+
+const addOne = (anyInput) => {
+    return  (parseFloat(anyInput) + 1);
+}
+
+const isEven = (anyInput) => {
+    if (parseFloat(anyInput) % 2 === 0) {
+        return true
+    } else {
+        return false;
+    }
+
+}
+
+const isIdentical = (arg1, arg2) => {
+    if (arg1 === arg2 && typeof arg1 === typeof arg2){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const isEqual = (arg1, arg2) => {
+    if (arg1 === arg2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const or = (arg1, arg2) => {
+    return (arg1 || arg2);
+}
+
+const  and = (arg1, arg2) => {
+    return(arg1 && arg2);
+}
+
+const concat = (arg1, arg2) => {
+    if (typeof arg1 && typeof arg2 === "string") {
+        return arg1 + arg2;
+
+    } else if (typeof arg1 && typeof arg2 === "number"){
+
+        return (arg1.toString()+arg2.valueOf());
+
+    }
+
+}
